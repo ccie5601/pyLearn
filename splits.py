708f899dc489cbc786a64e1baa1d251833a6688f
Aaron Smith <41849944+ccie5601@users.noreply.github.com>
2020-11-25 09:17:49 -0800

    Create splits.py

---
+++ splits.py
@@ -0,0 +1,11 @@
+# default splits at space
+variable.split()
+
+# split at the colon
+variable.split(':')
+
+# split at comma
+variable.split(',')
+
+# split at length of number x
+splits = variable[i:i+3] for i in range(0, len(variable), 3)])
